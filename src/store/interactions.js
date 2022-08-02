96a05e36c8543d941fd9cfca50cc3cadf6bbbf28
bspecv <farthingshane@gmail.com>
2022-08-02 13:21:40 -0400

    filled orders

--- src/store/interactions.js
+++ src/store/interactions.js
@@ -8,7 +8,9 @@ import {
   filledOrdersLoaded,
   allOrdersLoaded,
   orderCancelling,
-  orderCancelled
+  orderCancelled,
+  orderFilling,
+  orderFilled
 } from './actions'
 import Token from '../abis/Token.json'
 import Exchange from '../abis/Exchange.json'
@@ -80,7 +82,16 @@ export const loadAllOrders = async (exchange, dispatch) => {
   const allOrders = orderStream.map((event) => event.returnValues)
   // Add open orders to the redux store
   dispatch(allOrdersLoaded(allOrders))
-  
+}
+
+export const subscribeToEvents = async (exchange, dispatch) => {
+  exchange.events.Cancel({}, (error, event) => {
+    dispatch(orderCancelled(event.returnValues))
+  })
+
+  exchange.events.Trade({}, (error, event) => {
+    dispatch(orderFilled(event.returnValues))
+  })
 }
 
 export const cancelOrder = (dispatch, exchange, order, account) => {
@@ -90,14 +101,19 @@ exchange.methods.cancelOrder(order.id).send({from:account})
 })
 
 .on('error', (error) => {
-  console.log (error)
-  window.alert('There was an error!')
-})
+   console.log (error)
+   window.alert('There was an error!')
+ })
 }
 
-export const subscribeToEvents = async (exchange, dispatch) => {
-  exchange.events.Cancel({}, (error, event) => {
-    dispatch(orderCancelled(event.returnValues))
+export const fillOrder = (dispatch, exchange, order, account) => {
+  exchange.methods.fillOrder(order.id).send({ from: account })
+  .on('transactionHash', (hash) => {
+     dispatch(orderFilling())
+  })
+  .on('error', (error) => {
+    console.log(error)
+    window.alert('There was an error!')
   })
 }
 
